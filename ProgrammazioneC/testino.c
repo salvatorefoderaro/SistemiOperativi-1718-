d820fea357c84fdf48523499520981c1f39cba9c
Salvatore Foderaro <salvatore.foderaro@gmail.com>
2018-06-25 20:11:31 +0200

    Ruspa!

---
+++ ProgrammazioneC/testino.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]){
+
+	printf("Ho ricevuto %d argomenti. Il primo è: %s, mentre il secondo è: %s\n", argc, argv[0], argv[1]);
+
+
+}
